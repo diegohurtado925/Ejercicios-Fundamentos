a9336014144a72c4afad9af7300d2c5e371e999d
diegohurtado925 <164244994+diegohurtado925@users.noreply.github.com>
2024-05-06 12:39:18 -0600

    Add files via upload

---
+++ tabla12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int c = 1, r = 0;
+
+    while (c<=12)
+    {
+        r = 12*c;
+        cout << r << endl << endl;
+        c++;
+    }
+
+    return 0;
+}
